5d37e51258d4d7e6c859a61dddb9f4f353a6d878
ks-neerajn <134484079+ks-neerajn@users.noreply.github.com>
2023-09-08 22:20:23 +0530

    Add methods of delete and edit and changed color

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 
 const page = () => {
 
+  /** ALL STATE */
   const [todoTitle, setTodoTitle] = useState("");
   const [todoDescription, setTodoDescription] = useState("");
 
@@ -10,7 +11,6 @@ const page = () => {
     const localStorageTodos = localStorage.getItem("allTodos")
 
     if(localStorageTodos) {
-      console.log("This is reloading")
       return[...JSON.parse(localStorageTodos)];
     }
     return [];
@@ -22,6 +22,13 @@ const page = () => {
     }
   }, [allTodos]);
 
+  /** ALL FUNCTIONS */
+
+  /**
+   * Funtion to save todo in the array
+   * 
+   * @param {Event} e - the event of form submission
+   */
   function saveTodo(e) {
     e.preventDefault();
 
@@ -35,10 +42,33 @@ const page = () => {
 
   };
 
+  /**
+   * Function to edit the todo
+   * 
+   * @param {Number} index - the index of todo to be edited
+   */
+  function editTodo(index) {
+    const todo = allTodos[index];
+    setTodoTitle(todo.todoTitle);
+    setTodoDescription(todo.todoDescription);
+    deleteTodo(index);
+  }
+  
+  /**
+   * Function to delete the todo
+   * 
+   * @param {Number} index - the index of todo to be edited
+   */
+  function deleteTodo(index) {
+    const todos = [...allTodos];
+    todos.splice(index, 1);
+    setAllTodos(todos);
+  }
+
   
   return (
     <>
-      <h1 className='bg-green-800 text-xl text-white p-5 text-center'>
+      <h1 className='bg-zinc-800 text-xl text-white p-5 text-center'>
         React - Tailwind Todo List
       </h1>
       <form onSubmit={saveTodo} className='flex flex-col max-w-xl mx-auto justify-center mb-3'>
@@ -57,7 +87,7 @@ const page = () => {
         />
 
         <button
-          className='bg-green-500 hover:bg-green-700  text-white px-4 py-2 rounded-md w-60 mx-auto'  
+          className='bg-zinc-500 hover:bg-zinc-700  text-white px-4 py-2 rounded-md w-60 mx-auto'  
         >
           Add Todo
         </button>
@@ -69,16 +99,22 @@ const page = () => {
           ? 
       <div className='grid grid-cols-4 gap-3 border p-2 border-gray-50 bg-slate-50'>
         
-          {allTodos.map((todo) => (
-            <div key={todo.todoTitle} 
-              className='flex flex-col m-3 p-2 border border-green-200 bg-slate-100 rounded-md h-40 overflow-hidden hover:bg-slate-200'> 
+          {allTodos.map((todo, index) => (
+            <div key={index} 
+              className='flex flex-col m-3 p-2 border border-zinc-200 bg-slate-100 rounded-md h-40 overflow-hidden hover:bg-slate-200'> 
               <p className='text-2xl font-bold'>{todo.todoTitle}</p>
               <p className='text-sm font-medium'>{todo.todoDescription}</p>
               <div className='self-center mt-auto'>
-              <button className='px-2 border border-green-500 rounded-md hover:bg-green-500 hover:text-white mx-2'>
+              <button 
+                className='px-2 border border-zinc-500 rounded-md hover:bg-zinc-500 hover:text-white mx-2'
+                onClick={() => editTodo(index)}
+              >
                 Edit
               </button>
-              <button className='px-2 border border-red-500 rounded-md hover:bg-red-500 hover:text-white mx-2'>
+              <button 
+                className='px-2 border border-red-500 rounded-md hover:bg-red-500 hover:text-white mx-2'
+                onClick={() => deleteTodo(index)}
+              >
                 Delete
               </button>
               </div>
